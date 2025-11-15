71abae51c0c8c36306d64a4c773ae18170473385
Patrick <32450935+PaddyGameTV@users.noreply.github.com>
2025-11-15 03:06:05 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -17,7 +17,7 @@ fi
 if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
     rm -fr config defaultconfigs kubejs mods packmenu Server-Files-* neoforge*
 
-    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/7121/795/ServerFiles-$SERVER_VERSION.zip" || exit 9
+    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/7217/013/ServerFiles-$SERVER_VERSION.zip" || exit 9
    
     unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
     DIR_TEST="ServerFiles-$SERVER_VERSION"
