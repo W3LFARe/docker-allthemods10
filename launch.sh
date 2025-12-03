3b4ea5e9dbdf1d8ebec3fac482617b34c374b115
Gadoon <38638045+Gadoon@users.noreply.github.com>
2025-12-03 21:28:48 +0900

    Updated to 5.3

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@ set -x
 
 
 NEOFORGE_VERSION=21.1.215
-SERVER_VERSION=5.2
+SERVER_VERSION=5.3
 
 cd /data
 
@@ -17,7 +17,7 @@ fi
 if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
     rm -fr config defaultconfigs kubejs mods packmenu Server-Files-* neoforge*
 
-    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/7271/405/ServerFiles-$SERVER_VERSION.zip" || exit 9
+    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/7285/673/ServerFiles-$SERVER_VERSION.zip" || exit 9
    
     unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
     DIR_TEST="ServerFiles-$SERVER_VERSION"
