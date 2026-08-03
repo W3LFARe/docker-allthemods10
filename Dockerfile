5f9f5f2609169e581dce23bd3de7fc3b178eff64
Gadoon <38638045+Gadoon@users.noreply.github.com>
2026-08-03 12:17:26 -0500

    Updated to 7.3

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 FROM eclipse-temurin:21-jdk
 
-LABEL version="7.2"
+LABEL version="7.3"
 
 
 RUN apt-get update && apt-get install -y curl unzip jq && \
--- launch.sh
+++ launch.sh
@@ -2,8 +2,8 @@
 set -x
 
 
-NEOFORGE_VERSION=21.1.241
-SERVER_VERSION=7.2
+NEOFORGE_VERSION=21.1.247
+SERVER_VERSION=7.3
 
 cd /data
 
@@ -17,7 +17,7 @@ fi
 if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
     rm -fr config defaultconfigs kubejs mods packmenu Server-Files-* neoforge*
 
-    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/8469/489/ServerFiles-$SERVER_VERSION.zip" || exit 9
+    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/8558/557/ServerFiles-$SERVER_VERSION.zip" || exit 9
 
     unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
     DIR_TEST="ServerFiles-$SERVER_VERSION"
